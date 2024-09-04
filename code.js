6ebcf5aa27cca44c04de75f6c5d68dbce102d856
Abigail Riffle <abigailriffle@gmail.com>
2024-09-04 15:43:44 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,113 @@
 console.log("I can load the .js file");
 
-var Alice = 5;
+class Player{
+	constructor(isTagger, icon){
+		this.index = [0,0];
+		this.points = 0;
+		this.isTagger = isTagger;
+		this.icon = icon
+	}
+}
+
 console.log(" I can declare a variable");
+
+var minCoord = 0;
+var maxCoord = 50;
+
+var tagger;
+var runner;
+var enabledInput = false;
+
+
+//Initiate starting variables
+function On_Load(){
+  console.log("Started load");
+
+  tagger = new Player(true, document.getElementById("player-tagger"));
+  runner = new Player(false, document.getElementById("player-runner"));
+
+
+  tagger.index = [0,0];
+  runner.index = [0,10];
+
+  console.log("Successfully loaded");
+
+  enabledInput = true;
+  
+  Draw_Screen();
+}
+
+document.addEventListener('keydown', keyPress);
+
+function keyPress(){
+	const key = event.key;
+	console.log("Pressed a button");
+	switch (event.key) {		
+    		case "ArrowLeft":
+        		Left(tagger);
+        		break;
+    		case "ArrowRight":
+        		Right(tagger);
+        		break;
+    		case "ArrowUp":
+        		Up(tagger);
+       		 	break;
+    		case "ArrowDown":
+        		Down(tagger);
+        	break;
+		}
+}
+
+//Check for point conditions
+function CheckPoints(){
+  Check_is_bopped();
+  Check_time_points();
+}
+// Helper functions for point conditions
+function Check_is_bopped(){
+  tagger.points++;
+}
+function Check_time_points(){
+  runner.points++;
+}
+
+function Draw_Screen(){
+  tagger.icon.style.left = `$[tagger.icon.index[0]]px`;
+  runner.icon.style.left = `$[runner.icon.index[0]]px`;
+
+  tagger.icon.style.up = `$[tagger.icon.index[1]]px`;
+  runner.icon.style.up = `$[runner.icon.index[1]]px`;
+}
+
+function Left(activePlayer){
+  Move(activePlayer, [-1,0]);
+}
+function Right(activePlayer){
+  Move(activePlayer, [1,0]);
+}
+function Up(activePlayer){
+  Move(activePlayer, [0,1]);
+}
+function Down(activePlayer){
+  Move(activePlayer, [0,-1]);
+}
+
+function Move(activePlayer, deltaPosition){
+  activePlayer.index = [activePlayer.index[0]+deltaPosition[0], activePlayer.index[1]+deltaPosition[1]];
+  
+  //Figure out whether it is trying to go off the grid
+  if(activePlayer.index[0] < minCoord){
+    activePlayer.index[0] = minCoord;
+  }
+  if(activePlayer.index[0] > maxCoord){
+    activePlayer.index[0] = maxCoord;
+  }
+
+  if(activePlayer.index[1] < minCoord){
+    activePlayer.index[1] = minCoord;
+  }
+  if(activePlayer.index[1] > maxCoord){
+    activePlayer.index[1] = maxCoord;
+  }
+  Draw_Screen();
+}
