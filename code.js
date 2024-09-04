2681cf0a4c735d8d5fc310c19f783b20a5f2d78e
Abigail Riffle <abigailriffle@gmail.com>
2024-09-04 13:39:13 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,9 @@
 player_tagger = document.getElementById("player_tagger");
 player_runner = document.getElementById("player_runner");
 
+minCoord = 0;
+maxCoord = 50;
+
 //Initiate starting variables
 On_Load(){
   player_tagger.pos = [0,0];
@@ -34,4 +37,20 @@ Down(Player){
 }
 
 Move(Player, deltaPosition){
+  Player.pos = [Player.pos+deltaPosition[0], Player.pos+deltaPosition[1]];
+  
+  //Figure out whether it is trying to go off the grid
+  if(Player.pos[0] < minCoord){
+    Player.pos[0] = minCoord;
+  }
+  if(Player.pos[0] > maxCoord){
+    Player.pos[0] = maxCoord;
+  }
+
+  if(Player.pos[1] < minCoord){
+    Player.pos[1] = minCoord;
+  }
+  if(Player.pos[1] > maxCoord){
+    Player.pos[1] = maxCoord;
+  }
 }
