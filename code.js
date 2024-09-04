c6cce0c425437aae2189a7103318d95509361959
Abigail Riffle <abigailriffle@gmail.com>
2024-09-04 14:28:02 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,5 @@
 console.log("I can load the .js file");
 
-//Set up global variables
-// player_tagger = document.getElementById("player_tagger");
-// player_runner = document.getElementById("player_runner");
-
-// var minCoord = 0;
-// var maxCoord = 50;
 const PI = 3.141592653589793;
 
 console.log("I initialized static variables");
-
